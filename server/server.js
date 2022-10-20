802e532d2bbb2e633a93fce7645e4283ee6cc216
Ayodeji Komolafe <kvngeko@icloud.com>
2022-10-20 14:59:44 -0400

    rollbar added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: 'fd70263409c14f6bb66c2c4d051e8bae',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
